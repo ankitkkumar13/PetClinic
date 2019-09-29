72de3815c30026fb710f60ff0f4b1c803fbd7b42
ankit <ankitkpawar13@gmail.com>
2019-09-29 19:43:27 +0530

    owners and vet added

---
+++ pet-clinic-web/src/main/java/com/diro/sfgpetclicnic/controller/OwnerController.java
@@ -0,0 +1,24 @@
+package com.diro.sfgpetclicnic.controller;
+
+import com.diro.sfgpetclicnic.services.OwnerService;
+import com.diro.sfgpetclicnic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @RequestMapping({"", "/owners", "/owners/index", "/owners/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", ownerService.findAll());
+        return "vets/index";
+    }
+
+    public OwnerController(OwnerService ownerService) {
+        super();
+        this.ownerService = ownerService;
+    }
+}
